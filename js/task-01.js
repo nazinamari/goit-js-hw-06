02104217bb8a319e63c26b28879a8f2f3ec91760
nazinamari <93985053+nazinamari@users.noreply.github.com>
2022-07-05 17:38:58 +0300

    01

--- js/task-01.js
+++ js/task-01.js
@@ -3,18 +3,18 @@
 // 1. Посчитает и выведет в консоль количество категорий в ul#categories,
 // то есть элементов li.item.
 
-const ElementsList = document.querySelectorAll('li.item').length;
-console.log(`Number of categories: ${ElementsList}`);
+const countCategories = document.querySelectorAll('li.item').length;
+console.log(`Number of categories: ${countCategories}`);
 
-console.log(ElementsList);
+console.log(countCategories);
 
 // 2. Для каждого элемента li.item в списке ul#categories, 
 // найдет и выведет в консоль текст заголовка элемента(тега < h2 >) 
 // и количество элементов в категории(всех вложенных в него < li >).
 
 
-const test = document.querySelectorAll('li.item');
-test.forEach(element => {
+const CategoriesList = document.querySelectorAll('li.item');
+CategoriesList.forEach(element => {
     const h2El = element.querySelector('h2').textContent
     const ulEl = element.querySelectorAll('li')
     console.log(`Category: ${h2El}`)
