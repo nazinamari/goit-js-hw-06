17fc1cfad865c16d1afe2d7e817c927a2d369afb
nazinamari <93985053+nazinamari@users.noreply.github.com>
2022-07-05 23:12:15 +0300

    09

--- js/task-09.js
+++ js/task-09.js
@@ -1,6 +1,6 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
 
 // todo Напиши скрипт, который изменяет цвета фона элемента < body >
 //   через инлайн стиль при клике на button.change -
@@ -10,10 +10,27 @@ function getRandomHexColor() {
 //   <p>Background color: <span class="color">-</span></p>
 //   <button type="button" class="change-color">Change color</button>
 // </div>
+
 // Для генерации случайного цвета используй функцию getRandomHexColor.
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+const bodyRef = document.querySelector('body');
+
+const btnEl = document.querySelector('.change-color');
+
+const hexText = document.querySelector('.color');
+
+btnEl.addEventListener("click", onhexColor);
+
+function onhexColor() {
+  const hexColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = hexColor;
+  hexText.textContent = hexColor;
+  console.log(hexColor);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
