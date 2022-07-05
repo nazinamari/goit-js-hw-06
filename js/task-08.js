7395cd406ec4cc8633136a151d9cedc9f5630aa3
nazinamari <93985053+nazinamari@users.noreply.github.com>
2022-07-05 22:52:49 +0300

    08

--- js/task-08.js
+++ js/task-08.js
@@ -14,9 +14,33 @@
 
 // Обработка отправки формы form.login-form должна быть по событию submit.
 // При отправке формы страница не должна перезагружаться.
+
 // Если в форме есть незаполненные поля, выводи alert с предупреждением о том,
 // что все поля должны быть заполнены.
+
 // Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект,
 // где имя поля будет именем свойства, а значение поля - значением свойства.
+
 // Для доступа к элементам формы используй свойство elements.
 // Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', hadleSubmit);
+
+function hadleSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+
+    const mail = formElements.email.value;
+    const pass = formElements.password.value;
+
+    const formData = {
+        mail,
+        pass,
+    };
+
+    console.log(formData);
+
+    event.currentTarget.reset()
+
+}
