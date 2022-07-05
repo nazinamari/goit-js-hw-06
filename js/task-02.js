f7b6eef2f5c52838ac12092569f5c156a5ed6e71
nazinamari <93985053+nazinamari@users.noreply.github.com>
2022-07-05 18:00:27 +0300

    02

--- js/task-02.js
+++ js/task-02.js
@@ -11,12 +11,36 @@ const ingredients = [
 
 // Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
 
-
+// const ingredientsEl = document.createElement('li')
+// console.log(ingredientsEl);
 
 // Добавит название ингредиента как его текстовое содержимое.
 
+// ingredientsEl.textContent = "Name";
 
 // Добавит элементу класс item.
 
+// ingredientsEl.classList.add('ingredientsEl')
 
 // После чего вставит все <li> за одну операцию в список ul#ingredients.
+
+// const IngredientsList = document.querySelector('#ingredients');
+
+// IngredientsList.appendChild(ingredientsEl);
+
+// console.log(IngredientsList);
+
+
+const IngredientsList = document.querySelector('#ingredients');
+const array = ingredients.map(element => {
+  const newIngredientsEl = document.createElement('li');
+  newIngredientsEl.classList.add('ingridient-item');
+  newIngredientsEl.textContent = element;
+  return newIngredientsEl;
+});
+
+IngredientsList.append(...array);
+
+console.log(IngredientsList)
+
+
